d48bc55943063c05279b326da454b4273fc181a9
alvarozechlinski <alvaro.zechlinski@lealenergia.com.br>
2026-05-04 17:06:03 -0300

    lpbot_ricardozer

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => ({
         main: path.resolve(__dirname, "index.html"),
         programareformabr: path.resolve(__dirname, "programareformabr/index.html"),
         modelobot: path.resolve(__dirname, "modelobot/index.html"),
+        modelobot_ricadoz: path.resolve(__dirname, "modelobot_ricadoz/index.html"),
       },
     },
   },
